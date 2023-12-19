f9c8c8af92f6c851bd6153312b8385ce5c7cdc6c
Jose Jaime Come <josejaimecome@gmail.com>
2023-12-19 19:42:41 +0200

    Script to create sample and transform data updated, PowerBI Dashboard added

--- Data Transformation.R
+++ Data Transformation.R
@@ -2,6 +2,7 @@ library(plyr)
 library(dplyr)
 library(gdata)
 library(tidyr)
+library(data.table)
 
 # Get data in data folder and transform to be used in PowerBI
 dir <- "C:/Users/COME/Downloads/Master Degree/Coding/Analisis/Data Analisis/analytics_demographic_and_health_data/"
@@ -13,12 +14,30 @@ births <- read.csv(paste0(dir, "data/births.csv"))
 deaths <- read.csv(paste0(dir, "data/deaths.csv"))
 migration <- read.csv(paste0(dir, "data/migration.csv"))
 
-# Data transformation
+# Create DataFrame with Rounds number
+id <- seq(1, 20, by=1)
+description <- c("")
+registration_round_table <- data.frame(id, description)
 
 
-# End of data transfomation
+#======================================Data transformation==========================================================
+
+individuals <- as.data.table(individuals)
+# clmns=colnames(df)
+aggr <- individuals[,.N,by=household_id]
+individuals <- merge(individuals, aggr)
+
+# colnames(df)[17] <- "Members"
+colnames(individuals)[which(names(individuals) == "N")] <- "members"
+
+households <- merge(households, individuals[,c("id", "members")], by = "id", all.x = TRUE )
+migration$number <- 1
+
+#==================================End of data transfomation======================================================
+
 # dir.create(file.path(dir, "transformed_data"), showWarnings = FALSE)
 # Saving transformed data
+write.csv(registration_round_table, paste0(dir, "transformed_data/registration_round.csv"), row.names = FALSE)
 write.csv(households, paste0(dir, "transformed_data/transformed_households.csv"), row.names = FALSE)
 write.csv(individuals, paste0(dir, "transformed_data/transformed_individuals.csv"), row.names = FALSE)
 write.csv(births, paste0(dir, "transformed_data/transformed_births.csv"), row.names = FALSE)
--- Script to create sample data.R
+++ Script to create sample data.R
@@ -1,6 +1,7 @@
 # By this script, tables are created
 
 dir <- "path"
+
 # HouseHolds DataFrame
 id <- seq(1, 6000, by=1)
 admin_1 <- LETTERS[1:1]
@@ -18,7 +19,7 @@ households <- data.frame (
 )
 households$registration_date <- as.Date(households$registration_date, format = "%m/%d/%y") + households$registration_round * 180
 households$registration_update_date <- as.Date(households$registration_update_date, format = "%m/%d/%y") + households$registration_round * 180
-
+households$registration_round <- sample(6, size = nrow(households), replace = TRUE)
 households$household_code <- paste0(households$admin_1, "-", households$admin_2,"-", households$admin_1, "-", households$id)
 names(households)
 nrow(households)
@@ -48,8 +49,10 @@ individuals  <- data.frame(
 )
 individuals$registration_date <- as.Date(individuals$registration_date, format = "%m/%d/%y") + individuals$registration_round * 180
 individuals$registration_update_date <- as.Date(individuals$registration_update_date, format = "%m/%d/%y") + individuals$registration_round * 180
-
 individuals$gender <- ifelse(individuals$id >28000, "Female", individuals$gender)
+individuals$household_id <- sample(10, size = nrow(individuals), replace = TRUE)
+individuals$registration_round <- sample(6, size = nrow(individuals), replace = TRUE)
+
 head(individuals, 20)
 
 # Birth Dataframe
@@ -71,7 +74,7 @@ births$registration_round <- ifelse(births$id > 3200, 6, births$registration_rou
 births$date_of_birth <- as.Date(births$date_of_birth, format = "%m/%d/%y") + births$registration_round * 260
 births$registration_date <- as.Date(births$registration_date, format = "%m/%d/%y") + births$registration_round * 180
 births$registration_update_date <- as.Date(births$registration_update_date, format = "%m/%d/%y") + births$registration_round * 180
-
+births$registration_round <- sample(6, size = nrow(births), replace = TRUE)
 head(births)
 
 # Deaths Dataframe
@@ -91,12 +94,14 @@ deaths$registration_round <- ifelse(deaths$id > 4200, 6, deaths$registration_rou
 deaths$date_of_death <- as.Date(deaths$date_of_death, format = "%m/%d/%y") + deaths$registration_round * 260
 deaths$registration_date <- as.Date(deaths$registration_date, format = "%m/%d/%y") + deaths$registration_round * 180
 deaths$registration_update_date <- as.Date(deaths$registration_update_date, format = "%m/%d/%y") + deaths$registration_round * 180
+deaths$registration_round <- sample(6, size = nrow(deaths), replace = TRUE)
 
 # Migration Dataframe
 id <- seq(1, 10000, by=1)
 date_of_migration <- c("02/18/18")
 migration_type <- c("In")
 from_region <- c("yes")
+to_region <- c("yes")
 registration_round <- seq(1, 5, by=1)
 individual_id <- seq(1, 5000, by=1)
 registration_date <- c("02/18/18")
@@ -104,7 +109,7 @@ registration_update_date <- c("02/18/18")
 registration_user <- c("user_1")
 registration_update_user <- c("user_1")
 migration  <- data.frame(
-  id, first_name, last_name, date_of_migration, migration_type, from_region,
+  id, first_name, last_name, date_of_migration, migration_type, from_region, to_region,
   individual_id, registration_round, registration_date, registration_update_date, registration_user, 
   registration_update_user
   
@@ -112,8 +117,15 @@ migration  <- data.frame(
 migration$date_of_migration <- as.Date(migration$date_of_migration, format = "%m/%d/%y") + migration$registration_round * 260
 migration$registration_date <- as.Date(migration$registration_date, format = "%m/%d/%y") + migration$registration_round * 180
 migration$registration_update_date <- as.Date(migration$registration_update_date, format = "%m/%d/%y") + migration$registration_round * 180
+migration$migration_type <- ifelse((migration$id%%2) == 0, "In", "Out")
+migration$from_region <- ifelse((migration$id%%2) == 0, "Yes", "No")
+migration$to_region <- ifelse((migration$id%%2) == 0, "Yes", "No")
+migration$from_region <- ifelse(migration$migration_type == "Out", migration$from_region, "")
 
 migration$registration_round <- ifelse(migration$id > 9000, 6,migration$registration_round)
+migration$registration_round <- sample(6, size = nrow(migration), replace = TRUE)
+migration$migration_type <- ifelse(migration$id > 8500,"In" ,migration$migration_type)
+migration$to_region <- ifelse(migration$migration_type == "In", migration$to_region, "")
 
 # dir.create(file.path(dir, "data"), showWarnings = FALSE)
 write.csv(households, paste0(dir, "data/households.csv"), row.names = FALSE)
